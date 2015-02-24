6e3a50552af7b729cc88aeedeb5c2fbeabc4f23e
Shinya Sasaki <shinya.sasaki@kek.jp>
2015-02-25 08:06:50 +0900

    add neighbor triangle for triangle class

--- include/triangle.h
+++ include/triangle.h
@@ -8,23 +8,26 @@ using namespace std;
 
 class Triangle {
     private:
-        Node   *p[3];
-        bool   isExist;
+        Node     *p[3];
+        bool     isExist;
+        Triangle *neighbor_triangle[3];
     
     public:
         Triangle(){
             for(int i=0;i<3;i++){
-                p[i]     = NULL;
+                p[i] = NULL;
                 //p2[i]    = NULL;
-                //nei[i]   = NULL;
+                neighbor_triangle[i] = NULL;
                 isExist = false;
             }
         }
 
-        void  setNodes( Node*, Node*, Node* );
-        void  setExist( bool );
-        Node* getNode( unsigned int );
-        bool  getExist();
+        void      setNodes( Node*, Node*, Node* );
+        void      setExist( bool );
+        void      setNeighborTri( Triangle*, Triangle*, Triangle* );
+        Node*     getNode( unsigned int );
+        bool      getExist();
+        Triangle* getNeighborTri( int );
 };
 
 #endif /* end of include guard */
